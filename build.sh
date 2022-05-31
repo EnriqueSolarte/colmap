9374ae32a49044d0a85da72d4f84af5d14c5ed71
Enrique Solarte <enrique.solarte@itri.org.tw>
2022-05-31 13:53:16 +0800

    WIP: Creating build.sh file

--- build.sh
+++ build.sh
@@ -1,8 +1,50 @@
 #!/usr/bin/env bash
 
+# ! Installing dependecies
+sudo apt-get install \
+    git \
+    cmake \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libboost-test-dev \
+    libeigen3-dev \
+    libsuitesparse-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev
+
+#! Under Ubuntu 16.04/18.04 
+#! the CMake configuration scripts of CGAL are broken 
+# ! and you must also install the CGAL Qt5 package:
+sudo apt-get install libcgal-qt5-dev
+
+
+# ! building Ceres lib 
+cd ceres_2.1.0
 mkdir build
 cd build
-
-make -j4 
+cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
+make -j4
 sudo make install
+
+# ! Building COLMAP
+cd .. 
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+# mkdir build
+# cd build
+
+# make -j4 
+# sudo make install
 
